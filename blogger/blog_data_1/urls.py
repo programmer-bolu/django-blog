d43585b6ef73d98933594a192c0af9c7b7a3337f
Akingbade Boluwatife Ilerioluwa <akingabdeboluwatife030@gmail.com>
2024-07-16 15:52:34 +0100

    update final

--- blogger/blog_data_1/urls.py
+++ blogger/blog_data_1/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('<int:id>', views.blogpost, name='blogpost'),
 ]
--- blogger/blog_data_1/views.py
+++ blogger/blog_data_1/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from . import models
 # Create your views here.
 
 def home(request):
     posts = models.Post.objects.all()
     return render(request, 'index.html', {'posts': posts})
+
+def blogpost(request, id):
+    post = get_list_or_404(models.Post, id=id)
+    return render(request, 'blog.html', {'post': post})
